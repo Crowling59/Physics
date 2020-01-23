1f7ba98e14b1f24aeb0f6a4b9739786e2830c29d
Lenophie <quasiphies@gmail.com>
2020-01-23 11:23:40 +0100

    Add mouse camera controls

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -2,12 +2,18 @@
 
 namespace Controllers {
     public class PlayerController : MonoBehaviour {
+        [SerializeField] private float mouseSensitivity = 1f;
+        private float yawn = 0.0f;
+        private float pitch = 0.0f;
+
         [SerializeField] private float speed = 1f;
         [SerializeField] private BallSpawnerController ballSpawnerController = default;
         private Transform playerTransform;
 
         private void Awake() {
             playerTransform = transform;
+            yawn = 0.0f;
+            pitch = 0.0f;
         }
 
         private void Update() {
@@ -16,6 +22,12 @@ namespace Controllers {
                 currentPosition.x + Input.GetAxis("Horizontal") * speed,
                 currentPosition.y,
                 currentPosition.z + Input.GetAxis("Vertical") * speed);
+
+            yawn += Input.GetAxis("Mouse X") * mouseSensitivity;
+            pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+            if (pitch < -90f) pitch = -90f;
+            else if (pitch > 90f) pitch = 90f;
+            transform.eulerAngles = new Vector3(pitch, yawn, 0f);
 
             if (Input.GetKeyUp("space"))
                 ballSpawnerController.SpawnBall();
