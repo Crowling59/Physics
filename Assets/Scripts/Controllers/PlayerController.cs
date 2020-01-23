b0f1a91c6b6c935c543f12f0a1e2a15c6ee660a1
Lenophie <quasiphies@gmail.com>
2020-01-23 11:06:09 +0100

    Set player speed

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -2,6 +2,7 @@
 
 namespace Controllers {
     public class PlayerController : MonoBehaviour {
+        [SerializeField] private float speed = 1f;
         private Transform playerTransform;
 
         private void Awake() {
@@ -11,9 +12,9 @@ namespace Controllers {
         private void Update() {
             Vector3 currentPosition = playerTransform.position;
             playerTransform.position = new Vector3(
-                currentPosition.x + Input.GetAxis("Horizontal"),
+                currentPosition.x + Input.GetAxis("Horizontal") * speed,
                 currentPosition.y,
-                currentPosition.z + Input.GetAxis("Vertical"));
+                currentPosition.z + Input.GetAxis("Vertical") * speed);
         }
     }
 }
