c50f77104e30182eccb2581386961a04e3be6ffd
Lenophie <quasiphies@gmail.com>
2020-01-23 11:39:58 +0100

    Add RagdollPartController

---
+++ Assets/Scripts/Controllers/RagdollPartController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+    public class RagdollPartController : MonoBehaviour {
+        private static LayerMask ballLayer;
+
+        private void Awake() {
+            ballLayer = LayerMask.NameToLayer("Ball");
+        }
+
+        private void OnCollisionEnter(Collision other) {
+            if (other.gameObject.layer == ballLayer)
+                Destroy(gameObject);
+        }
+    }
+}
