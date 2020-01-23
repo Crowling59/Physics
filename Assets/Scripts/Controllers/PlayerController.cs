30b14696ca7e2b0e99d53638ed77fbebf47bc471
Lenophie <quasiphies@gmail.com>
2020-01-23 10:58:22 +0100

    Add PlayerController

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+    public class PlayerController : MonoBehaviour {
+        private Transform playerTransform;
+
+        private void Awake() {
+            playerTransform = transform;
+        }
+
+        private void Update() {
+            Vector3 currentPosition = playerTransform.position;
+            playerTransform.position = new Vector3(
+                currentPosition.x + Input.GetAxis("Horizontal"),
+                currentPosition.y,
+                currentPosition.z + Input.GetAxis("Vertical"));
+        }
+    }
+}
