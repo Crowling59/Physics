664596bad2db3e6b106c9f17a3087d425e37d1b8
Lenophie <quasiphies@gmail.com>
2020-01-23 11:53:51 +0100

    Add RagdollController

---
+++ Assets/Scripts/Controllers/RagdollController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Controllers {
+    public class RagdollController : MonoBehaviour {
+        private Rigidbody ragdollRigidbody;
+        [SerializeField] private float speedModifier = 0.01f;
+
+        private void Awake() {
+            ragdollRigidbody = GetComponent<Rigidbody>();
+            StartCoroutine(DoMovements());
+        }
+
+        private IEnumerator DoMovements() {
+            while (true) {
+                float timeToDoMovement = Random.Range(2f, 5f);
+                float timer = 0f;
+                Vector3 deltaPosition = new Vector3(Random.Range(-1f, 1f),
+                                            Random.Range(-0.1f, 0.1f),
+                                            Random.Range(-1f, 1f)) * speedModifier;
+
+                while (timer < timeToDoMovement) {
+                    timer += Time.deltaTime;
+                    ragdollRigidbody.position += deltaPosition;
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+    }
+}
